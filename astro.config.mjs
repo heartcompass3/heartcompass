4d52cd6036f734539ac5089ae3c84f056725bf9a
heartcompass <heartcompass3@gmail.com>
2025-12-26 12:10:40 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import keystatic from '@keystatic/astro';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://heartcompass.co.il',
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     tailwind({ applyBaseStyles: false }),
     keystatic(),
