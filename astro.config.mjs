2a192877c369fa0fa85a6155086252b20e604a1b
heartcompass <heartcompass3@gmail.com>
2025-12-29 06:50:57 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-
-  integrations: [
-    keystatic(),
-  ],
+  integrations: [react(), markdoc(), keystatic()],
 });
