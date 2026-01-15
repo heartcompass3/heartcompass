1799ff377e7385f1018bfd1203b32dea5ec06aa2
heartcompass <heartcompass3@gmail.com>
2026-01-15 19:03:05 +0200

    fix(sanity): remove duplicate blockContent and stabilize schemaTypes

--- studio2/schemaTypes/documents/index.ts
+++ studio2/schemaTypes/documents/index.ts
@@ -1,14 +1,22 @@
-// studio2/schemaTypes/documents/index.ts
-import page from "./page";
-import author from "./author";
-import methodPage from "./methodPage";
-import homePage from "./homePage";
-import article from "./article";
+import page from './page'
+import siteSettings from './siteSettings'
+import homePage from './homePage'
+import methodPage from './methodPage'
+import contactPage from './contactPage'
+
+import article from './article'
+import author from './author'
 
 export const documents = [
-  page,
+  siteSettings,
   homePage,
   methodPage,
+  contactPage,
+
+  // Content
   article,
   author,
-];
+
+  // Generic pages
+  page,
+]
--- studio2/schemaTypes/index.ts
+++ studio2/schemaTypes/index.ts
@@ -1,4 +1,8 @@
-import { documents } from "./documents";
-import { objects } from "./objects";
+// studio2/schemaTypes/index.ts
+import { documents } from './documents'
+import { objects } from './objects'
 
-export const schemaTypes = [...documents, ...objects];
+export const schemaTypes = [
+  ...documents,
+  ...objects,
+]
