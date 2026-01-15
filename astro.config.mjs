81e19a9d57c111e082019f377b3be969b6c770dd
heartcompass <heartcompass3@gmail.com>
2026-01-15 19:19:24 +0200

    chore(seo): enable sitemap generation for search engines

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel/serverless'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
+  site: 'https://heartcompass.vercel.app',
   output: 'server',
   adapter: vercel(),
+  integrations: [sitemap()],
 })
