f9dc0a9eedc431829092cefaecc3a4996f5e8879
heartcompass <heartcompass3@gmail.com>
2026-04-06 17:28:48 +0300

    Update rss.xml.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,15 +3,16 @@ import { getCollection } from 'astro:content';
 
 export async function GET() {
   const articles = await getCollection('articles');
+  
   return rss({
     title: "מצפן הלב - יוסי מדלסי",
     description: "ליווי רגשי-קוגניטיבי לשחרור דפוסים אוטומטיים",
-    site: 'https://heartcompass.vercel.app', // הגדרת כתובת ישירה כאן
+    site: 'https://heartcompass.vercel.app',
     items: articles.map((post) => ({
       title: post.data.title || "מאמר",
       pubDate: post.data.pubDate || new Date(),
       description: post.data.description || "",
-      link: `/articles/${post.slug}/`,
+      link: `/articles/${post.slug}`,
     })),
   });
 }
