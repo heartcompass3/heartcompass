fac39af5f62c07b66a11a470e1a8e72c905c544b
heartcompass <heartcompass3@gmail.com>
2026-04-23 15:50:54 +0300

    Update sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,9 +1,13 @@
 import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
-const projectId = import.meta.env.PUBLIC_SANITY_PROJECT_ID || process.env.PUBLIC_SANITY_PROJECT_ID
-const dataset = import.meta.env.PUBLIC_SANITY_DATASET || process.env.PUBLIC_SANITY_DATASET
-const apiVersion = '2026-04-23' // השתמשנו בתאריך של היום!
+const projectId =
+  import.meta.env.PUBLIC_SANITY_PROJECT_ID || process.env.PUBLIC_SANITY_PROJECT_ID
+const dataset =
+  import.meta.env.PUBLIC_SANITY_DATASET || process.env.PUBLIC_SANITY_DATASET
+
+// סלילת דרך חדשה עם תאריך של היום
+const apiVersion = '2026-04-23'
 
 if (!projectId) throw new Error('Missing PUBLIC_SANITY_PROJECT_ID')
 if (!dataset) throw new Error('Missing PUBLIC_SANITY_DATASET')
@@ -12,7 +16,9 @@ export const sanity = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false, // חייב להיות false!
+  
+  // סילוק המטמון לחלוטין כדי להכריח את האתר לקרוא את שורת ההסכמה
+  useCdn: false,
 })
 
 const builder = imageUrlBuilder(sanity)
